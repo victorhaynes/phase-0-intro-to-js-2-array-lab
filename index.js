1302f5fe637721e43aa9acf60f252f8e2485950b
Victor Haynes <haynesvic@gmail.com>
2022-05-21 18:14:56 -0500

    commit tree cf4662591b84de731bca1d93a2d6729c0203ba50

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
 // Write your solution here!
+
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+
+////////////Destructively add new cat name to end of cat Array
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+//Re-initialize cats Array before running next operation
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+
+
+////////////Destructively add new cat name to beginning of cat Array
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+//Re-initialize cats Array before running next operation
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+
+////////////Destrutively remove last cat from cat Array
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+};
+//Re-initialize cats Array before running next operation
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+
+
+////////////Destrutively remove first cat from cat Array
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+};
+//Re-initialize cats Array before running next operation
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+
+
+
+////////////Append a cat to the cats Array, leave cat Array unchanged
+let copyOfcats = []
+
+function appendCat(name){
+    copyOfcats = [...cats,name]
+    return(copyOfcats)
+};
+
+
+
+//////////Prepend a cat to the cats Array, leave cat Array unchanged
+let copyOfcatsPre = []
+
+function prependCat(name){
+    copyOfcatsPre = [name,...cats]
+    return(copyOfcatsPre)
+};
+
+//////////Remove last cat in cats Array, leave cat Array unchanged
+let noLastCat =[]
+function removeLastCat(name){
+    noLastCat = cats.slice(0, cats.length - 1 )
+    return(noLastCat)
+};
+
+//////////Remove first cat from the cats Array, leave cat Array unchanged
+let noFirstCat =[]
+function removeFirstCat(name){
+    noFirstCat = cats.slice(1)
+    return(noFirstCat)
+};
